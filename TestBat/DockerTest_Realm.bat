08d45b1f9e55cea2deab9a3443786e22168f9b2d
zhoubaoliang <zhoubaoliang@boke.com>
2025-02-25 14:23:20 +0800

    文件移动

--- TestBat/DockerTest_Realm.bat
+++ TestBat/DockerTest_Realm.bat
@@ -1,2 +1,2 @@
-cd /d %~dp0Bin
+cd ../Bin
 App.exe  --AppType=TestDocker --SceneType=Realm --Process=1 --StartConfig=StartConfig/LocalHost --Console=1
--- TestBat/DockerTest_RouterManager.bat
+++ TestBat/DockerTest_RouterManager.bat
@@ -1,2 +1,2 @@
-cd /d %~dp0Bin
+cd ../Bin
 App.exe  --AppType=TestDocker --SceneType=RouterManager --Process=1 --StartConfig=StartConfig/LocalHost --Console=1
