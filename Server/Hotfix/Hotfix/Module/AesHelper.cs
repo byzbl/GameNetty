fb70b6bd60447e13182626a99bda4758ef866945
zhoubaoliang <zhoubaoliang@boke.com>
2025-02-27 14:44:45 +0800

    update

--- Server/Hotfix/Hotfix/Module/AesHelper.cs
+++ Server/Hotfix/Hotfix/Module/AesHelper.cs
@@ -107,22 +107,5 @@ namespace ET
         }
     }
 
-    public static class RSAHelper
-    {
-        public static string RSAEncrypt(string text, byte[] pemKey)
-        {
-            RsaKeyParameters publicKeyParam = (RsaKeyParameters)PublicKeyFactory.CreateKey(pemKey);
-
-            string XML = string.Format("<RSAKeyValue><Modulus>{0}</Modulus><Exponent>{1}</Exponent></RSAKeyValue>",
-                Convert.ToBase64String(publicKeyParam.Modulus.ToByteArrayUnsigned()),
-                Convert.ToBase64String(publicKeyParam.Exponent.ToByteArrayUnsigned()));
-
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-            byte[] cipherbytes;
-            rsa.FromXmlString(XML);
-            cipherbytes = rsa.Encrypt(Encoding.UTF8.GetBytes(text), false);
-
-            return Convert.ToBase64String(cipherbytes);
-        }
-    }
+  
 }
---
+++ Server/Hotfix/Hotfix/Module/RSAHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace ET
+{
+    public static class RSAHelper
+    {
+        public static string RSAEncrypt(string text, byte[] pemKey)
+        {
+            RsaKeyParameters publicKeyParam = (RsaKeyParameters)PublicKeyFactory.CreateKey(pemKey);
+
+            string XML = string.Format("<RSAKeyValue><Modulus>{0}</Modulus><Exponent>{1}</Exponent></RSAKeyValue>",
+                Convert.ToBase64String(publicKeyParam.Modulus.ToByteArrayUnsigned()),
+                Convert.ToBase64String(publicKeyParam.Exponent.ToByteArrayUnsigned()));
+
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            byte[] cipherbytes;
+            rsa.FromXmlString(XML);
+            cipherbytes = rsa.Encrypt(Encoding.UTF8.GetBytes(text), false);
+
+            return Convert.ToBase64String(cipherbytes);
+        }
+    }
+}
+
